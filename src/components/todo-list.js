0ed6502748b84640530ebce24f5ef41a2d3bb292
Vladyslav <34074565+Vladyslav-Plakhuta@users.noreply.github.com>
2018-03-05 22:50:59 +0200

    Add files via upload

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -10,21 +10,36 @@ export default class TodoListComponent {
   mount() {
     this.mountPoint.innerHTML = this.render();
     this.querySelectors();
+    this.doneTask();
   }
 
   addTask(task) {
     this.list.innerHTML += `
-            <li class="todo-list__item">${task}</li>
+    <tr><td><input class ="checkbox" type="checkbox"></td>
+    <td>${task}</td>
+    <td><button onclick="removeTask(this)" value="&#215" style="width:5px;height:15px;"></button></td></tr>
         `;
   }
 
+  doneTask() {
+    var checkboxElements = document.getElementsByClassName("checkbox");
+
+    for (var i = 0; i < checkboxElements.length; i++) {
+      checkboxElements[i].addEventListener("change", function() {
+        if (this.checked) {
+          this.closest("tr").style.textDecoration = "line-through";
+        }
+      });
+    }
+  }
   getNumTasks() {
     return this.list.children.length;
   }
 
   render() {
     return `
-      <ul class="todo-list"></ul>
+      <br>
+      <table class="todo-list" style="width:50px"></table>
     `;
   }
 }
